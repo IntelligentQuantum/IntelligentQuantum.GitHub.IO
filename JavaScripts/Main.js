f30d75641e80b5aecd890a2a2c2f876d51ab91ae
Parsa <parsa.firoozi55@gmail.com>
2021-11-29 14:04:32 +0330

    feat(Aside): DiscordConnection

---
+++ JavaScripts/Main.js
@@ -0,0 +1,35 @@
+let DiscordID = "550013353171484682",
+    userStatus = document.getElementById("aside__user--status"),
+    userAvatar = document.getElementById("aside__user--image");
+
+
+const DiscordConnection = async () =>
+{
+    try
+    {
+        const response = await axios.get('https://persian-discord-api.herokuapp.com/user?id=' + DiscordID + '&json=true');
+
+
+        userAvatar.src = response.data.userAvatar;
+
+        if (response.data.userPresence === 'dnd')
+        {
+            userStatus.classList.add('bg-red');
+        }
+        else if (response.data.userPresence === 'online')
+        {
+            userStatus.classList.add('bg-green');
+        }
+        else if (response.data.userPresence === 'offline')
+        {
+            userStatus.classList.add('bg-gray');
+        }
+    }
+    catch (error)
+    {
+        console.error(error);
+    }
+}
+
+DiscordConnection().then(() => { console.log('Discord Connection Function !') })
+setInterval(() => { DiscordConnection().then(() => { console.log('Discord Connection Function !') }) }, 10000);
