b173b06713dce2e797e702f57f6c9573918f4bce
Parsa <parsa.firoozi55@gmail.com>
2021-11-29 18:46:19 +0330

    feat(Aside): DiscordConnection

--- JavaScripts/Main.js
+++ JavaScripts/Main.js
@@ -1,13 +1,15 @@
-const DiscordID = "550013353171484682",
-    userStatus = document.getElementById("aside__user--status"),
-    userAvatar = document.getElementById("aside__user--image");
+const DiscordID = "550013353171484682";
+const URL = `https://persian-discord-api.herokuapp.com/user?id=${DiscordID}&json=true`;
+
+const userStatus = document.getElementById("aside__user--status");
+const userAvatar = document.getElementById("aside__user--image");
 
 
 const DiscordConnection = async () =>
 {
     try
     {
-        const response = await axios.get(`https://persian-discord-api.herokuapp.com/user?id=${DiscordID}&json=true`);
+        const response = await axios.get(URL);
 
 
         userAvatar.src = response.data.userAvatar;
@@ -32,5 +34,5 @@ const DiscordConnection = async () =>
     }
 }
 
-DiscordConnection().then(() => { console.log('Discord Connection Function !') })
+DiscordConnection().then(() => { console.log('Discord Connection Function !') });
 setInterval(() => { DiscordConnection().then(() => { console.log('Discord Connection Function !') }) }, 1000 * 60 * 10);
