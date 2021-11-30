773035b4266702c2483c8139b1af8551964c2315
Parsa <parsa.firoozi55@gmail.com>
2021-11-30 13:27:54 +0330

    feat(Aside&Navbar): Navbar & ProgressAnimation

--- JavaScripts/Main.js
+++ JavaScripts/Main.js
@@ -2,20 +2,18 @@
 
 const DiscordID = "550013353171484682";
 const URL = `https://persian-discord-api.herokuapp.com/user?id=${DiscordID}&json=true`;
-const container = document.getElementById("mainContainer");
-const userAvatar = document.getElementById("asideUserImage");
+const container = document.querySelector(".container");
+const userAvatar = document.querySelector(".aside__user--image");
+const userStatus = document.querySelector(".aside__user--status");
 
-const DiscordConnection = async () =>
-{
-    try
-    {
+const DiscordConnection = async () => {
+    try {
         const response = await axios.get(URL);
 
         userAvatar.src = response.data.userAvatar;
         userAvatar.alt = response.data.userName;
 
-        switch (response.data.userPresence)
-        {
+        switch (response.data.userPresence) {
             case 'dnd':
                 userStatus.classList.add('bg-red');
                 break;
@@ -31,25 +29,27 @@ const DiscordConnection = async () =>
             default:
                 userStatus.classList.add('bg-gray');
         }
-    }
-    catch (error)
-    {
+    } catch (error) {
         console.error(error);
     }
 }
 
-DiscordConnection().then(() => { console.log('Discord Connection Function !') });
-setInterval(() => { DiscordConnection().then(() => { console.log('Discord Connection Function !') }) }, 1000 * 60 * 10);
+DiscordConnection().then(() => {
+    console.log('Discord Connection Function !')
+});
+setInterval(() => {
+    DiscordConnection().then(() => {
+        console.log('Discord Connection Function !')
+    })
+}, 1000 * 60 * 10);
 
 
 // NavBar
 
-const nav = document.getElementById("nav");
-const navHamburger = document.getElementById("navHamburger");
-const userStatus = document.getElementById("asideUserStatus");
+const nav = document.querySelector(".nav");
+const navHamburger = document.querySelector(".nav__hamburger");
 
-navHamburger.addEventListener("click", () =>
-{
+navHamburger.addEventListener("click", () => {
     container.classList.toggle('navbarOpen');
     nav.classList.toggle('open');
     navHamburger.classList.toggle('open');
