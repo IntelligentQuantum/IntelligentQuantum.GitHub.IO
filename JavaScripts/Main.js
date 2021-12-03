e2a8e2055d0b697f0a5c838e78c0dabfa495e017
IntelligentQuantum <IntelligentQuantum@ProtonMail.Com>
2021-12-03 17:32:17 +0330

    feat(Base): Responsive Part 1

--- JavaScripts/Main.js
+++ JavaScripts/Main.js
@@ -1,56 +1,5 @@
-// DiscordConnection
-const DiscordID = '550013353171484682';
-const URL = `https://persian-discord-api.herokuapp.com/user?id=${DiscordID}&json=true`;
-const container = document.querySelector('.container');
-const userAvatar = document.querySelector('.aside__user--image');
-const userStatus = document.querySelector('.aside__user--status');
-
-const DiscordConnection = async () =>
-{
-    try
-    {
-        const response = await axios.get(URL);
-
-        userAvatar.src = response.data.userAvatar;
-        userAvatar.alt = response.data.userName;
-
-        switch (response.data.userPresence)
-        {
-            case 'dnd':
-                userStatus.classList.add('bg-red');
-                break;
-            case 'online':
-                userStatus.classList.add('bg-green');
-                break;
-            case 'idle':
-                userStatus.classList.add('bg-yellow');
-                break;
-            case 'offline':
-                userStatus.classList.add('bg-gray');
-                break;
-            default:
-                userStatus.classList.add('bg-gray');
-        }
-    }
-    catch (error)
-    {
-        console.error(error);
-    }
-}
-
-DiscordConnection().then(() =>
-{
-    console.log('Discord Connection Function !')
-});
-setInterval(() =>
-{
-    DiscordConnection().then(() =>
-    {
-        console.log('Discord Connection Function !')
-    })
-}, 1000 * 60 * 10);
-
 // NavBar
+const container = document.querySelector('.container');
 const nav = document.querySelector('.nav');
 const navHamburger = document.querySelector('.nav__hamburger');
 
