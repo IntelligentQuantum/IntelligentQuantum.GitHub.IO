9c33cd3214dfde8a18dc4b737bfe0197b7e646fc
im-parsa <parsa.firoozi55@gmail.com>
2022-06-08 15:01:33 +0430

    feat(server)

---
+++ server.js
@@ -0,0 +1,46 @@
+const next = require('next');
+const { parse } = require('url');
+const { createServer } = require('http');
+
+const hostname = process.env.HOST;
+const port = process.env.PORT || 3000;
+const dev = process.env.NODE_ENV !== 'production';
+
+const app = next({ dev, hostname, port });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() =>
+{
+    createServer(async (req, res) =>
+    {
+        try
+        {
+            const parsedUrl = parse(req.url, true);
+            const { pathname, query } = parsedUrl;
+
+            if (pathname === '/a')
+            {
+                await app.render(req, res, '/a', query)
+            }
+            else if (pathname === '/b')
+            {
+                await app.render(req, res, '/b', query)
+            }
+            else
+            {
+                await handle(req, res, parsedUrl)
+            }
+        }
+        catch (err)
+        {
+            console.error('Error occurred handling', req.url, err)
+            res.statusCode = 500
+            res.end('internal server error')
+        }
+    })
+        .listen(port, (err) =>
+        {
+            if (err) throw err
+            console.log(`> Ready on http://${hostname}:${port}`)
+        })
+})
