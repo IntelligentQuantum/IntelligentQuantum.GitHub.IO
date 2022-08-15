d5cfa8c4f84eea16a622d6a5efc99964ac7e4dc7
im-parsa <parsa.firoozi55@gmail.com>
2022-08-16 00:37:25 +0430

    feat(Core/Pages): Document - schema structured data and seo

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -17,13 +17,14 @@ class MyDocument extends Document
                     <link rel='icon' href='https://parsa-firoozi.ir/static/images/favicon.png'/>
                     <link rel='apple-touch-icon' href='https://parsa-firoozi.ir/static/images/favicon.png'/>
                     <link rel='manifest' href='https://parsa-firoozi.ir/static/manifest.json'/>
+                    <link rel='canonical' href="https://parsa-firoozi.ir" />
 
                     <meta property='theme-color' content='#5294E2'/>
 
                     <meta name='language' content='en'/>
                     <meta name='Classification' content='Portfolio'/>
                     <meta name='subject' content='Parsa Firoozi Full-Stack Developer & Graphic Designer'/>
-                    <meta name='description' content='Parsa Firoozi Full-Stack Developer & Graphic Designer'/>
+                    <meta name='description' content='Parsa Firoozi Full-Stack Developer & Graphic Designer. He focuses on JavaScript-related technologies like Typescript, NodeJS, DenoJS, NestJS, NextJS, React and React Native. He is also an experienced graphic designer and proficient in Adobe Photoshop, Adobe Illustrator, Adobe Premiere Pro and Gimp softwares.'/>
                     <meta name='keywords' content='im-parsa, Parsa Firoozi, Parsa, Firoozi, Portfolio, Full-Stack Developer, Graphic Designer'/>
                     <meta name='author' content='im-parsa'/>
 
@@ -49,6 +50,29 @@ class MyDocument extends Document
                             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
                             })(window,document,'script','dataLayer', 'GTM-${ process.env.GTM_ID }');`
+                        }}
+                    />
+
+                    <Script
+                        id='schema-structured-data'
+                        type="application/ld+json"
+                        dangerouslySetInnerHTML={{
+                            __html: JSON.stringify(
+                                {
+                                    '@context': 'https://schema.org/',
+                                    '@type': 'Recipe',
+                                    'name': 'Parsa Firoozi',
+                                    'author':
+                                        {
+                                            '@type': 'Person',
+                                            'name': 'Parsa Firoozi'
+                                        },
+                                    'email': 'mailto:hello@parsa-firoozi.ir',
+                                    'image': 'https://parsa-firoozi.ir/static/images/favicon.png',
+                                    'jobTitle': 'Full-Stack Developer',
+                                    'url': 'https://www.parsa-firoozi.ir'
+                                }
+                            )
                         }}
                     />
                 </Head>
